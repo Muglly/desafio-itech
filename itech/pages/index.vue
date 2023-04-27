b8f3114905e5f99d0deea43d32cb157309e44e35
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-04-27 12:46:26 -0300

    Create page login

--- itech/pages/index.vue
+++ itech/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="flex justify-center align-middle">
-    <input placeholder="Email" type="email" />
-    <input placeholder="Senha" type="password" />
-    <button>Entrar</button>
-    <a>Registar</a>
+  <div class="flex justify-center items-center bg-slate-800 min-h-screen">
+    <div class="flex flex-col gap-3 box-border w-80 text-center">
+      <input class="rounded p-2" placeholder="Email" type="email" />
+    <input class="rounded p-2" placeholder="Senha" type="password" />
+    <button type="submit" class="rounded p-2 bg-sky-800 hover:bg-sky-600 text-white">Entrar</button>
+    <a class="text-orange-500 hover:text-orange-700" href="register">Cadastre-se</a>
+    </div>
   </div>
 </template>
 
