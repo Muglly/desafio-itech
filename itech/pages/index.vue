c019f24f231f943e92edc2e902743b861aafdac7
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-04-28 13:28:21 -0300

    Add form

--- itech/pages/index.vue
+++ itech/pages/index.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="flex justify-center items-center bg-slate-800 min-h-screen">
-    <div class="flex flex-col gap-3 box-border w-80 text-center">
+  <main class="flex justify-center items-center bg-slate-800 min-h-screen">
+    <form class="flex flex-col gap-3 box-border w-80 text-center">
       <h2 class="text-white font-bold text-xl">Faça o seu login</h2>
       <input class="rounded p-2" placeholder="Email" type="email" />
       <input class="rounded p-2" placeholder="Senha" type="password" />
-      <button type="submit" class="rounded p-2 bg-sky-800 hover:bg-sky-600 text-white">Entrar</button>
-      <a class="text-orange-500 hover:text-orange-700" href="register">Cadastre-se</a>
-    </div>
-  </div>
+      <button
+        type="submit"
+        class="rounded p-2 bg-sky-800 hover:bg-sky-600 text-white"
+      >
+        Entrar
+      </button>
+      <a class="text-orange-500 hover:text-orange-700" href="register"
+        >Cadastre-se</a
+      >
+    </form>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
-
--- itech/pages/register.vue
+++ itech/pages/register.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="flex justify-center items-center bg-slate-800 min-h-screen">
-    <div class="flex flex-col gap-3 box-border w-80 text-center">
+  <main class="flex justify-center items-center bg-slate-800 min-h-screen">
+    <form class="flex flex-col gap-3 box-border w-80 text-center">
       <h2 class="text-white font-bold text-xl">Faça o seu cadastro</h2>
       <input class="rounded p-2" placeholder="Name" type="name" />
       <input class="rounded p-2" placeholder="Email" type="email" />
-    <input class="rounded p-2" placeholder="Senha" type="password" />
-    <button type="submit" class="rounded p-2 bg-sky-800 hover:bg-sky-600 text-white">Cadastrar</button>
-
-    </div>
-  </div>
+      <input class="rounded p-2" placeholder="Senha" type="password" />
+      <button
+        type="submit"
+        class="rounded p-2 bg-sky-800 hover:bg-sky-600 text-white"
+      >
+        Cadastrar
+      </button>
+    </form>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'RegisterPage'
-}
+  name: "RegisterPage",
+};
 </script>
