e749b8fd81fdd235fb0ce9f83828a31aba4c1812
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-04-28 23:22:19 -0300

    Add login and singOut function with firebase auth

--- itech/pages/dashboard.vue
+++ itech/pages/dashboard.vue
@@ -1,5 +1,53 @@
 <template>
   <main class="h-screen flex justify-center">
-    <header class="bg-slate-800 text-white h-12 w-full">Dashboard</header>
+    <header class="bg-slate-800 text-white h-12 w-full flex justify-center">
+      <div class="flex justify-between items-center w-3/4">
+        <h1 class="text-white font-bold text-xl">Deshboard</h1>
+        <button
+          v-if="user"
+          @click="singout"
+          class="rounded p-2 bg-sky-800 hover:bg-sky-600 text-white px-10 py-1 font-bold"
+          type="button"
+        >
+          Sair
+        </button>
+      </div>
+    </header>
   </main>
 </template>
+
+<script>
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export default {
+  name: "DeshboardPage",
+
+  data() {
+    return {
+      user: "",
+    };
+  },
+
+  mounted() {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
+
+      this.user = user;
+    });
+  },
+
+  methods: {
+    singout() {
+      firebase
+        .auth()
+        .signOut()
+        .then((result) => {
+          console.log(result);
+          this.user = "";
+          this.$router.push("/");
+        });
+    },
+  },
+};
+</script>
--- itech/pages/index.vue
+++ itech/pages/index.vue
@@ -1,9 +1,22 @@
 <template>
   <main class="flex justify-center items-center bg-slate-800 min-h-screen">
-    <form class="flex flex-col gap-3 box-border w-80 text-center">
+    <form
+      @submit.prevent="pressed"
+      class="flex flex-col gap-3 box-border w-80 text-center"
+    >
       <h2 class="text-white font-bold text-xl">Faça o seu login</h2>
-      <input class="rounded p-2" placeholder="Email" type="email" />
-      <input class="rounded p-2" placeholder="Senha" type="password" />
+      <input
+        class="rounded p-2"
+        placeholder="Email"
+        type="email"
+        v-model="email"
+      />
+      <input
+        class="rounded p-2"
+        placeholder="Senha"
+        type="password"
+        v-model="password"
+      />
       <button
         type="submit"
         class="rounded p-2 bg-sky-800 hover:bg-sky-600 text-white"
@@ -18,7 +31,34 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/auth";
+
 export default {
   name: "IndexPage",
+
+  data() {
+    return {
+      email: "",
+      password: "",
+      errors: "",
+    };
+  },
+
+  methods: {
+    pressed() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then((user) => {
+          console.log(user);
+          this.$router.push("/dashboard");
+        })
+        .catch((error) => {
+          this.errors = error;
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
